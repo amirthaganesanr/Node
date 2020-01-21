4801dbd4d7d36183b72fa89471a50f4699baf864
amirthaganesan.ranga <amirthaganesan.ranganathan@lloydsbanking.com>
2020-01-21 13:41:01 +0000

    EXPRESS COMMIT

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+let http = require("http");
+
+const implementation = (request, response) => {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.end('Hello World\n');
+}
+
+http.createServer(implementation).listen(8081);
+
+console.log('Server running at http://127.0.0.1:8081');
