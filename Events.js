9eb3e47ff80249f96bc6692f65f842f15c5cbd16
amirthaganesan.ranga <amirthaganesan.ranganathan@lloydsbanking.com>
2020-01-21 14:19:39 +0000

    MULTIPLE LISTENERS TO EVENTS

--- Events.js
+++ Events.js
@@ -6,13 +6,20 @@ const connectionFn = () => {
     console.log('Connection Successful');
     eventEmitter.emit('data');
 }
+const connectionFn2 = () => {
+    console.log('Connection Successful for second event');
+}
 
 const dataFn = () => {
     console.log('Data loaded successfully');
 }
 
+//Every time event is bound with the function, it chains the function and repeats it
 eventEmitter.on('connection', connectionFn);
+eventEmitter.on('connection', connectionFn2);
+eventEmitter.addListener('connection', connectionFn);
 
+console.log('List of listeners for connection event \n',eventEmitter.listeners('connection'));
 eventEmitter.on('data', dataFn);
 
 eventEmitter.emit('connection');
